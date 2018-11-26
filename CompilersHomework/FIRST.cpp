d31c3c4a862b5e4b7db712a3322f678854f230d3
crablet <love4uandgzfc@gmail.com>
2018-11-27 01:28:39 +0800

    Added FIRST.cpp

---
+++ CompilersHomework/FIRST.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <cctype>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    std::unordered_map<char, std::vector<std::string>> Grammar;
+    std::unordered_map<char, std::unordered_set<char>> FIRST;
+    for (int i = 0; i < n; ++i)
+    {
+        std::string Str;
+        std::cin >> Str;
+        Grammar[Str[0]].push_back(Str.substr(3, Str.size()));
+        FIRST[Str[0]] = {};
+    }
+
+    while (true)
+    {
+        bool Changed = false;
+        for (const auto &r : Grammar)
+        {
+            auto Now = r.first;
+            auto BeforeSize = FIRST[Now].size();
+            for (const auto &X : r.second)
+            {
+                if (std::isupper(X.front()))
+                {
+                    for (const auto &t : FIRST[X.front()])
+                    {
+                        FIRST[Now].insert(t);
+                    }
+                    if (FIRST[X.front()].count('~') && X.size() > 1)
+                    {
+                        std::size_t i;
+                        for (i = 1; i < X.size(); ++i)
+                        {
+                            if (FIRST[X[i]].count('~'))
+                            {
+                                for (const auto &t : FIRST[X[i]])
+                                {
+                                    FIRST[Now].insert(t);
+                                }
+                            }
+                            else
+                            {
+                                for (const auto &t : FIRST[X[i]])
+                                {
+                                    FIRST[Now].insert(t);
+                                }
+
+                                break;
+                            }
+                        }
+                        if (i != X.size())
+                        {
+                            FIRST[Now].erase('~');
+                        }
+                    }
+                }
+                else if (X.front() == '~')
+                {
+                    FIRST[Now].insert('~');
+                    if (X.size() > 1)
+                    {
+                        for (const auto &t : FIRST[X[1]])
+                        {
+                            FIRST[Now].insert(t);
+                        }
+                        if (X.size() > 2 && FIRST[X[1]].count('~'))
+                        {
+                            for (std::size_t i = 1; i < X.size(); ++i)
+                            {
+                                if (FIRST[X[i]].count('~'))
+                                {
+                                    for (const auto &t : FIRST[X[i]])
+                                    {
+                                        FIRST[Now].insert(t);
+                                    }
+                                }
+                                else
+                                {
+                                    for (const auto &t : FIRST[X[i]])
+                                    {
+                                        FIRST[Now].insert(t);
+                                    }
+
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (std::islower(X.front()))
+                {
+                    FIRST[Now].insert(X.front());
+                }
+            }
+
+            Changed |= BeforeSize != FIRST[Now].size();
+        }
+
+        if (!Changed)
+        {
+            break;
+        }
+    }
+
+    for (const auto &rr : FIRST)
+    {
+        std::cout << rr.first << ":\n";
+        for (const auto &r : rr.second)
+        {
+            std::cout << r << ' ';
+        }
+        std::cout << '\n' << std::endl;
+    }
+
+    return 0;
+}
