80722da99f1cdf003cdd22fe7ca480edc66edd76
crablet <love4uandgzfc@gmail.com>
2019-08-06 21:56:13 +0800

    A simple Gaussian blur demo.

---
+++ GaussianBlur/GaussianBlur.cpp
@@ -0,0 +1,80 @@
+﻿#include "GaussianBlur.h"
+
+double GaussianBlur::GaussianFunction(int x) const
+{
+	return C * std::exp(-1.0 * x * x / (2.0 * Sigma * Sigma));
+}
+
+void GaussianBlur::GetKernal()
+{
+	double Sum = 0.0;
+
+	// 获取线性权值空间Weight[]
+	for (int i = 0; i < 2 * r + 1; ++i)
+	{
+		Weight[i] = GaussianFunction(i - r);
+		Sum += Weight[i];
+	}
+
+	// 归一化
+	for (int i = 0; i < 2 * r + 1; ++i)
+	{
+		Weight[i] /= Sum;
+	}
+}
+
+// 处理边缘像素越界的问题
+int GaussianBlur::DealEdges(int x, int i, int w) const
+{
+	int t = x + i;
+	if (Q_UNLIKELY(t < 0 || t >= w))	// [unlikely] in C++20
+	{
+		return x - i;
+	}
+	else
+	{
+		return t;
+	}
+}
+
+void GaussianBlur::Blur()
+{
+	for (int y = 0; y < OriginImage.height(); ++y)
+	{
+		for (int x = 0; x < OriginImage.width(); ++x)
+		{
+			double Red = 0.0, Green = 0.0, Blue = 0.0;
+			for (int i = -r; i <= r; ++i)
+			{
+				auto Rgb = QColor(OriginImage.pixelColor(DealEdges(x, i, OriginImage.width()), y));
+				Red += Weight[r + i] * Rgb.red();
+				Green += Weight[r + i] * Rgb.green();
+				Blue += Weight[r + i] * Rgb.blue();
+			}
+
+			FirstPassImage.setPixelColor(QPoint(x, y), QColor(Red, Green, Blue));
+		}
+	}
+
+	for (int y = 0; y < FirstPassImage.height(); ++y)
+	{
+		for (int x = 0; x < FirstPassImage.width(); ++x)
+		{
+			double Red = 0.0, Green = 0.0, Blue = 0.0;
+			for (int i = -r; i <= r; ++i)
+			{
+				auto Rgb = QColor(FirstPassImage.pixelColor(x, DealEdges(y, i, FirstPassImage.height())));
+				Red += Weight[r + i] * Rgb.red();
+				Green += Weight[r + i] * Rgb.green();
+				Blue += Weight[r + i] * Rgb.blue();
+			}
+
+			SecondPassImage.setPixelColor(QPoint(x, y), QColor(Red, Green, Blue));
+		}
+	}
+}
+
+GaussianBlur::operator QPixmap()
+{
+	return QPixmap::fromImage(SecondPassImage);
+}
---
+++ GaussianBlur/GaussianBlur.h
@@ -0,0 +1,47 @@
+﻿#ifndef GAUSSIANBLUR_H
+#define GAUSSIANBLUR_H
+
+#include <cmath>
+
+#include <QPixmap>
+#include <QImage>
+#include <QPoint>
+#include <QColor>
+#include <QtGlobal>
+
+class GaussianBlur
+{
+public:
+	GaussianBlur() = delete;
+	GaussianBlur(const QPixmap &Src, int r = 6, double Sigma = 4.0)
+		: OriginImage{ Src.toImage() },
+		  FirstPassImage{ Src.size(), QImage::Format_RGB32 }, SecondPassImage{ Src.size(), QImage::Format_RGB32 },
+		  r{ r }, Weight{ new double[2 * r + 1] },
+		  Sigma{ Sigma }, C{ 1 / (Sigma * std::sqrt(2 * std::acos(-1.0))) }
+	{
+		GetKernal();
+		Blur();
+	}
+
+	~GaussianBlur()
+	{
+		delete[] Weight;
+	}
+
+private:
+	QImage OriginImage, FirstPassImage, SecondPassImage;
+	int r;	// 高斯模糊半径
+	double *Weight;	// 权重空间
+	double Sigma, C;	// 高斯函数中的两个参数
+
+public:
+	operator QPixmap();
+
+private:
+	double GaussianFunction(int x) const;	// 一维高斯函数
+	void GetKernal();	// 获取线性权值空间
+	int DealEdges(int x, int i, int w) const;
+	void Blur();
+};
+
+#endif // GAUSSIANBLUR_H
